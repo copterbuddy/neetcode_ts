2659c00d802c98c51e9e6b83bff7f1bfd15b8a24
copterbuddy <copterbuddy@gmail.com>
2025-05-16 21:15:33 +0700

    add jestjs config

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let helloText = 'Learn React'
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {helloText}
         </a>
       </header>
     </div>
