738257c2eaffd0a500921fe85c606a30811895b8
copterbuddy <copterbuddy@gmail.com>
2025-05-24 20:57:33 +0700

    binary search fix bug

--- src/NeetCodeExam/LeetCode704BinarySearch/LeetCode704BinarySearch.spec.tsx
+++ src/NeetCodeExam/LeetCode704BinarySearch/LeetCode704BinarySearch.spec.tsx
@@ -1,9 +1,17 @@
 import search from "./LeetCode704BinarySearch";
 
-test('search success case 1', () => {
+test('search case found', () => {
     let nums: number[] = [-1,0,3,5,9,12]
     let target: number = 9
     let result: number = search(nums,target)
     let want: number = 4
+    expect(result).toEqual(want)
+});
+
+test('search case not found', () => {
+    let nums: number[] = [-1,0,3,5,9,12]
+    let target: number = 2
+    let result: number = search(nums,target)
+    let want: number = -1
     expect(result).toEqual(want)
 });
