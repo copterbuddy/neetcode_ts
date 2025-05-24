45195d327d83357ab25aeef48239a81eb70f5b05
copterbuddy <copterbuddy@gmail.com>
2025-05-24 16:51:50 +0700

    add carfleet solution implemented

--- src/NeetCodeExam/LeetCode853CarFleet.tsx
+++ src/NeetCodeExam/LeetCode853CarFleet.tsx
@@ -1,27 +1,27 @@
 function carFleet(target: number, position: number[], speed: number[]): number {
-    let totalCar = position.length
+    let totalCar: number = position.length
     let zip: [number, number][] = []
 
-    for(let i: number = 0; i < totalCar; i++){
+    for (let i: number = 0; i < totalCar; i++){
         zip.push([position[i], speed[i]])
     }
 
     zip.sort((a, b) => a[0] - b[0])
 
     let time: number[] = []
-    for(let i: number = 0; i < totalCar; i++){
+    for (let i: number = 0; i < totalCar; i++){
         time.push((target - position[i]) / speed[i])
     }
 
     let result: number = 0
-    let cur: number = Number.NEGATIVE_INFINITY
+    let curr: number = Number.NEGATIVE_INFINITY
     for(let i: number = totalCar - 1; i >= 0; i--){
-        if(time[i] > cur){
-            cur = time[i]
+        if(time[i] > curr){
+            curr = time[i]
             result++
         }
     }
-
+    
     return result
 };
 
