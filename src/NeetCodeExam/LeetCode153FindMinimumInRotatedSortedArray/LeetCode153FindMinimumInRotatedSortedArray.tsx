e885780300803f453227d1b47f3abee03f41dc9c
copterbuddy <copterbuddy@gmail.com>
2025-06-12 10:38:06 +0700

    leetcode153 fixbug

--- src/NeetCodeExam/LeetCode153FindMinimumInRotatedSortedArray/LeetCode153FindMinimumInRotatedSortedArray.tsx
+++ src/NeetCodeExam/LeetCode153FindMinimumInRotatedSortedArray/LeetCode153FindMinimumInRotatedSortedArray.tsx
@@ -1,21 +1,16 @@
 export default function findMin(nums: number[]): number {
-    let l = 0
-    let r = nums.length - 1
-    let res = nums[0]
+    let l = 0;
+    let r = nums.length - 1;
 
-    while(l < r){
-        if(nums[l] < nums[r]){
-            return Math.min(res, nums[l])
-        }
+    while (l < r) {
+        const m = Math.floor((l + r) / 2);
 
-        let m = Math.floor((l + r) / 2)
-        res = Math.min(nums[m], res)
-        if(nums[l] < nums[m]){
-            l = m + 1
-        }else{
-            r = m - 1
+        if (nums[m] > nums[r]) {
+            l = m + 1;
+        } else {
+            r = m;
         }
     }
 
-    return res
+    return nums[l];
 };
